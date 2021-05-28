2d634523ecf15ebf7b383bf42da9937c9ea96ac9
Stackbit Code Editor <projects@stackbit.com>
2021-05-28 00:08:17 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -31,7 +31,7 @@ export default class HeroSection extends React.Component {
                     )}
                     {_.get(section, 'subtitle', null) && (
                     <div className="section__copy">
-                      <p>{_.get(section, 'subtitle', null)}</p>
+                      <p style={{color: "gainsboro"}}>{_.get(section, 'subtitle', null)}</p>
                     </div>
                     )}
                     {_.get(section, 'actions', null) && (
