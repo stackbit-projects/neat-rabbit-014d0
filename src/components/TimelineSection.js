4b36b5a65716107616fa8c846c631dc35e8dd091
Stackbit Code Editor <projects@stackbit.com>
2021-06-10 23:33:26 +0000

    TimelineSection.js: changed

--- src/components/TimelineSection.js
+++ src/components/TimelineSection.js
@@ -7,18 +7,7 @@ export default class TimelineSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
-            <section className="section">
-              <div className="container container--md">
-                {_.get(section, 'title', null) && (
-                <h2 className="section__title align-center">{_.get(section, 'title', null)}</h2>
-                )}
-                {_.get(section, 'content', null) && (
-                <div className="section__copy">
-                  {markdownify(_.get(section, 'content', null))}
-                </div>
-                )}
-              </div>
-            </section>
+            
         );
     }
 }
