2ac278b79fee47ba963ae443af62e8994240288f
Saarthak Sarup <saarthak.sarup@gmail.com>
2021-06-11 18:01:22 -0700

    Update TimelineSection.js
    added iframe link

--- src/components/TimelineSection.js
+++ src/components/TimelineSection.js
@@ -7,7 +7,7 @@ export default class TimelineSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
-            <iframe src=>
+            <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1LNhfuBuHPIW7cYumNSG9FrKvo8rmZn0LrfL8io5-HW8&font=Default&lang=en&start_at_end=true&debug=true&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
         );
     }
 }
