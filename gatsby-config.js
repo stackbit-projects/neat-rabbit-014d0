1820f6f0e39ef8544123a17ca255460343921b6a
saarthaks <saarthak.sarup@gmail.com>
2021-09-30 12:56:26 -0700

    restoring config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,18 +8,6 @@ module.exports = {
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
         {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [{
-                    resolve: `gatsby-remark-katex`,
-                    options: {
-                        // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-                        strict: `ignore`
-                    }
-                }]
-            }
-        },
-        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `pages`,
